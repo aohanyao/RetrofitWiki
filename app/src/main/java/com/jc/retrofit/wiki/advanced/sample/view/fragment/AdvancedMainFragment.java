3b65727124a98b4a6835f899a4d19e941d4baa9c
江俊超 <928692385@qq.com>
2019-01-30 23:25:23 +0800

    change list sort

--- app/src/main/java/com/jc/retrofit/wiki/advanced/sample/view/fragment/AdvancedMainFragment.java
+++ app/src/main/java/com/jc/retrofit/wiki/advanced/sample/view/fragment/AdvancedMainFragment.java
@@ -4,6 +4,9 @@ import com.jc.retrofit.wiki.advanced.error.view.HandlerResponseErrorActivity;
 import com.jc.retrofit.wiki.advanced.sample.view.activity.*;
 import com.jc.retrofit.wiki.base.BaseListFragment;
 import com.jc.retrofit.wiki.bean.TargetDummyItem;
+import com.jc.retrofit.wiki.sample.view.activity.LoggingInterceptorGetRequestActivity;
+import com.jc.retrofit.wiki.sample.view.activity.SampleGetRequestActivity;
+import com.jc.retrofit.wiki.sample.view.activity.SampleRxJavaGetRequestActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +21,15 @@ public class AdvancedMainFragment extends BaseListFragment {
     @Override
     protected List<TargetDummyItem> initTargets() {
         List<TargetDummyItem> datas = new ArrayList<>();
+        datas.add(new TargetDummyItem("Sample GET  ",
+                "原生的@GET请求，包含@Path和@Query，使用GsonConverterFactory进行序列化",
+                SampleGetRequestActivity.class));
+        datas.add(new TargetDummyItem("Sample RxJava2 GET  ",
+                "使用RxJava2进行配合请求，RxAndroid做线程切换,RxJava2CallAdapterFactory做数据适配",
+                SampleRxJavaGetRequestActivity.class));
+        datas.add(new TargetDummyItem("Logging Interceptor RxJava2 GET  ",
+                "加入RequestLoggingInterceptor，对网络请求进行打印",
+                LoggingInterceptorGetRequestActivity.class));
 
         datas.add(new TargetDummyItem("RxJavaObserveOnMainThread",
                 "不使用RxAndroid做线程切换，自己通过集成CallAdapter.Factory实现线程切换",
